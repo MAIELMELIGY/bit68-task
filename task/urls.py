a3fc16e6892b3fe615500d5d5068270dedd47d6b
MAIELMELIGY <mairshad238@gmail.com>
2022-01-14 01:01:17 +0200

    edit

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from task.views import ProductList 
+from task.views import ProductListcreate ,Listproduct 
 urlpatterns=[
-    path("productlist/",ProductList.as_view(),name="productlist")
+    path("productlistcreate/",ProductListcreate.as_view(),name="productlistcreate"),
+    path("Listproduct/",Listproduct.as_view(),name="Listproduct"),
 ]
--- task/views.py
+++ task/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import OrderingFilter 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.authentication import BasicAuthentication
-class ProductList(generics.ListCreateAPIView):
+class ProductListcreate(generics.ListCreateAPIView):
 	queryset = Product.objects.all()
 	serializer_class = ProductSerializer
 	filter_backends =[OrderingFilter,DjangoFilterBackend]
@@ -13,9 +13,11 @@ class ProductList(generics.ListCreateAPIView):
 	filterset_fields = ['created_by']
 	authentication_classes = [BasicAuthentication]
 	permission_classes = [IsAuthenticated]
+
 class Listproduct(generics.ListAPIView):
 	queryset = Product.objects.all()
 	serializer_class = ProductSerializer
+	filter_backends =[OrderingFilter,DjangoFilterBackend]
 	ordering_fields = ["price"]
 	filterset_fields = ['created_by']
 
